9b447b3de54acd43e94cca7ac930f07f7f988e96
hua123an <1994843581@qq.com>
2023-11-03 12:48:56 +0800

    add some academic sources

--- js/header.js
+++ js/header.js
@@ -2,3 +2,19 @@
 $(function () {
     $(".topNav").load("/components/header.html");
 })
+//set can't open console
+// Disable browser default right-click menu
+document.oncontextmenu = function (event) {
+    event.preventDefault()
+}
+// Forbid pressing F12
+document.onkeydown = document.onkeyup = document.onkeypress = function (event) {
+    const e = event || window.event || arguments.callee.caller.arguments[0]
+
+    if (e && e.keyCode == 123) {
+        e.returnValue = false
+        return false
+    }
+}
+
+
